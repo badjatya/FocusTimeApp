d7931a797855cf828e4e32dd8ee3dc0872057963
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-05 15:11:15 +0530

    size Constant created

---
+++ App/constants/sizes.js
@@ -0,0 +1,19 @@
+// Font Size
+export const fontSize = {
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxx: 80,
+};
+
+// Spacing for padding and margins
+export const spacing = {
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxx: 80,
+};
