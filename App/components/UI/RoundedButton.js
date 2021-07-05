ca931133335578adf4b7871ca7d1523ac8fd9a24
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-05 14:41:04 +0530

    RoundedButton Component styled

---
+++ App/components/UI/RoundedButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const RoundedButton = ({
+  style = {},
+  textStyle = {},
+  size = 125,
+  ...props
+}) => {
+  return (
+    <TouchableOpacity style={[styles(size).radius, style]}>
+      <Text style={[styles(size).text, textStyle]}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default RoundedButton;
+
+const styles = (size) =>
+  StyleSheet.create({
+    radius: {
+      borderRadius: size / 2,
+      width: size,
+      height: size,
+      alignItems: "center",
+      justifyContent: "center",
+      borderColor: "#fff",
+      borderWidth: 2,
+    },
+    text: {
+      fontSize: size / 3,
+      color: "#fff",
+    },
+  });
